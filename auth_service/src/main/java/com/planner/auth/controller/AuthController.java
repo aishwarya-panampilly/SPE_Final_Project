381a320d1ae4133c63dd6ae05cdadfd76a4dfbc2
aishwarya-panampilly <aishwarya.panampilly@gmail.com>
2025-05-19 13:26:33 +0530

    trial 23

--- auth_service/src/main/java/com/planner/auth/controller/AuthController.java
+++ auth_service/src/main/java/com/planner/auth/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RequestMapping("/auth")
 @CrossOrigin(origins = "*") // Allow cross-origin requests
 public class AuthController {
-
+//hello
     @Autowired
     private AuthService authService;
 
