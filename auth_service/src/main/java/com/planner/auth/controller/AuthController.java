1845851b6f3889aab2bd4091dd4ac39de2900a80
aishwarya-panampilly <aishwarya.panampilly@gmail.com>
2025-05-19 22:45:44 +0530

    executing pipeline

--- auth_service/src/main/java/com/planner/auth/controller/AuthController.java
+++ auth_service/src/main/java/com/planner/auth/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RequestMapping("/auth")
 @CrossOrigin(origins = "*") // Allow cross-origin requests
 public class AuthController {
-
+//executing pipeline
     @Autowired
     private AuthService authService;
 
